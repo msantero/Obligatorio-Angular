41f4a74d179f13b588089e25f83b78e01a581058
msantero <msantero@montevideo.com.uy>
2021-09-13 13:26:01 -0300

    1 file modified, 1 added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { usuarios } from '../usuarios';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
---
+++ src/app/usuarios.ts
@@ -0,0 +1,27 @@
+export interface Usuario {
+  id: number;
+  name: string;
+  password: string;
+  token: string;
+}
+
+export const usuarios = [
+  {
+    id: 1,
+    name: 'Marcos',
+    password: '123',
+    token: 'dfgdfg'
+  },
+  {
+    id: 1,
+    name: 'Pedro',
+    password: '456',
+    token: 'sdfsdffff'
+  }
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
