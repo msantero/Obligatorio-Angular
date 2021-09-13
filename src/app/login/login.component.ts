03d690e8bde6c9e50b5e2b0b11b21d6f722643f7
msantero <msantero@montevideo.com.uy>
2021-09-13 16:53:33 -0300

    3 files modified, 3 added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioAlertsComponent } from '../usuario-alerts/usuario-alerts.component';
 import { usuarios } from '../usuarios';
 
 @Component({
@@ -7,7 +8,23 @@ import { usuarios } from '../usuarios';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  nombre: string;
+  password: string;
+
   constructor() {}
 
   ngOnInit() {}
+
+  login() {
+    console.log(this.nombre);
+    console.log(this.password);
+  }
+
+  onNotify() {
+    window.alert('We notify you');
+  }
+
+  //si existe el usuario:
+  //<app-usuario-alerts [usuario]="usuario" (notify)="onNotify()">
+  //</app-usuario-alerts>
 }
