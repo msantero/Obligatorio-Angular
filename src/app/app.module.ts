fd09752f5da52e76670473a0267110b71838094c
msantero <msantero@montevideo.com.uy>
2021-09-13 13:02:11 -0300

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: LoginComponent },
+      { path: 'registro', component: RegistroComponent },
+      { path: 'dashboard', component: DashboardComponent }
+      //  { path: '', component: ProductListComponent },
+      //  { path: 'product/:productId', component: ProductDetailComponent },
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    DashboardComponent
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
